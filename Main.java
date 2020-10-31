197291f35c5d5f60bd0235ba5e5956dea4a3f189
April <51252578+ayypril@users.noreply.github.com>
2020-10-31 16:23:59 -0500

    NEW: actual multiline comments, and another method

--- Main.java
+++ Main.java
@@ -1,7 +1,31 @@
-class Main{
+import java.util.Random;
+
+class Main {
     public static void main(String[] args) {
-        //TODO: add other methods, etc
-        // ideas: 
+        /*TODO: add other methods, etc
+
+        ideas:
+        - random number generator
+        - random letter
+        - random item from array
+
+
+         */
         System.out.println("Hello World");
+
+
+        System.out.println("Testing:");
+        System.out.println(randomInt());
+    }
+
+    /**
+     * returns a random number from 0 to 100.
+     * uses java.util.Random
+     * @return
+     */
+    public static int randomInt(){
+        Random r = new Random();
+        int randInt = r.nextInt(100);
+        return randInt;
     }
 }
